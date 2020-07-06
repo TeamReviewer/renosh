876fadccee65b6407a20cec53dd4f2441852b1de
Eunji Kim <eunk@microsoft.com>
2020-07-06 10:42:59 +0900

    add page routing logic

--- src/components/InfoPage/Info.js
+++ src/components/InfoPage/Info.js
@@ -1,31 +1,31 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
+import { useHistory } from 'react-router-dom';
 
-class Info extends Component {
-
-    state = {
-        imageUrl: 'https://image.yes24.com/goods/49855699/800x0',
-        book : {
-            title: "어린왕자",
-            description: "프랑스 공군 비행사이자 작가인 앙투안 드 생텍쥐페리(Antoine de Saint-Exupéry)가 1943년 발표한 소설로 줄거리는 어느 사막 한가운데에 불시착한 조종사가 어린 왕자를 만나 왕자의 이야기를 듣게 된다는 내용이다."
+function Info(){
+    const history = useHistory();
+    const [bookInfo, setBookInfo] = useState(
+        {
+            imageUrl: 'https://image.yes24.com/goods/49855699/800x0',
+            book : {
+                title: "어린왕자",
+                description: "프랑스 공군 비행사이자 작가인 앙투안 드 생텍쥐페리(Antoine de Saint-Exupéry)가 1943년 발표한 소설로 줄거리는 어느 사막 한가운데에 불시착한 조종사가 어린 왕자를 만나 왕자의 이야기를 듣게 된다는 내용이다."
+            }
         }
+    )
+
+    function moveToNextPage() {
+        // move to /book page
+        history.push("/book");
     }
 
-    moveToNextPage(){
-        // Update Browser Routing
-        
-    }
-
-    
-    render() {
-        return(
-            <div id="Info">
-                <img src={this.state.imageUrl } alt="The Little Prince" width="500" height="600"/>
-                <p>{this.state.book.title}</p>
-                <p>{this.state.book.description}</p>
-                <button onClick={()=>this.moveToNextPage()}>Start Reading</button>
-            </div>
-        )
-    }
+    return(
+        <div id="Info">
+            <img src={bookInfo.imageUrl } alt="The Little Prince" width="500" height="600"/>
+            <p>{bookInfo.book.title}</p>
+            <p>{bookInfo.book.description}</p>
+            <button onClick={moveToNextPage}>Start Reading</button>
+        </div>
+    )
 }
 
 export default Info;
