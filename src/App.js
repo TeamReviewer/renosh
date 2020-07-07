922710223bbbe4cdb188d73c154baf07be58784d
Eunji Kim <eunk.angie@outlook.com>
2020-07-07 12:49:21 +0900

    Add book Info page (#3)
    * update book info page
    
    * add start reading button
    
    * add page routing logic
    
    Co-authored-by: Eunji Kim <eunk@microsoft.com>

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter,Route, Link,Switch } from "react-router-dom";
 import './App.css';
 import ListLayoutPage from './components/ListPage/ListLayout'
 import ViewerBody from './components/ViewerPage/ViewerBody'
+import Info from './components/InfoPage/Info'
 
 class App extends Component {
 
@@ -14,7 +15,7 @@ class App extends Component {
           <Switch>
             <Route path="/book" component={ViewerBody} ></Route>
             <Route exact path="/" component={ListLayoutPage} ></Route>            
-
+            <Route path="/bookInfo" component={Info} ></Route>
           </Switch>          
         </div>
       </BrowserRouter>
