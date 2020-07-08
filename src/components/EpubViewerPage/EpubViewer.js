269d9e39f4c1a6e3dbe71425bdebdd25ffc5345a
ljh95 <ilegwmam@gmail.com>
2020-07-08 14:19:54 +0900

    Highlighting in renosh App by using react-reader

--- src/components/EpubViewerPage/EpubViewer.js
+++ src/components/EpubViewerPage/EpubViewer.js
@@ -7,6 +7,34 @@ import {
   } from "react-reader";
 import "./EpubViewer.css"
 class EpubViewer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullscreen: false,
+      location: 2,
+      localFile: null,
+      localName: null,
+      largeText: false
+    };
+    this.rendition = null;
+  }
+
+  getRendition = rendition => {
+    // Set inital font-size, and add a pointer to rendition for later updates
+    const { largeText } = this.state;
+    this.rendition = rendition;
+    rendition.themes.fontSize(largeText ? "140%" : "100%");
+
+    // Apply a class to selected text
+    this.rendition.on("selected", function (cfiRange, contents) {
+      rendition.annotations.highlight(cfiRange, {}, (e) => {
+        console.log("highlight clicked", e.target);
+      });
+      contents.window.getSelection().removeAllRanges();
+
+    });
+
+  };
 
 
   render() {
@@ -17,6 +45,7 @@ class EpubViewer extends Component {
             title={"Alice in Wonderland"}
             location={"2"}
             locationChanged={epubcifi => console.log(epubcifi)}
+            getRendition={this.getRendition}
         />
       </div>
     );
