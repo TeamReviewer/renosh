9d8d5677b37fc3741bb2f96d92d8a45f9a6397bd
ljh95 <40597662+ljh95@users.noreply.github.com>
2020-08-02 18:12:55 +0900

    442/jongho/my page (#21)
    * add react-reader to render epub file
    
    * Highlighting in renosh App by using react-reader
    
    * when dragging text the moment findgit add *
    
    * remove Toc, solve Route
    
    * add Panel btn, func
    
    * some css change
    
    * add prev, next button
    
    * little ui update
    
    * add store
    
    * 405-listPage에서 받은 데이터를 스토어에 저장
    
    * updating infoPage...
    
    * 407 책 하나의 introPage가 열릴 수 있도록 하였습니다.
    
    * 뭔지 모르는 launch.json이라는 파일을 서버에 올맂 않게 ignore했습니다.
    
    * update epubviewer
    
    * 1차 릴리즈 완성
    
    * add autofocus and open Panel
    
    * 442/jongho/myPage components frame create
    
    Co-authored-by: Eunji Kim <eunk@microsoft.com>

---
+++ src/components/MyPageContent/MyAnnoList.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MyAnnoList extends Component {
+    render() {
+        return (
+            <div>
+                3. myAnnoList Contents!!
+            </div>
+        )
+    }
+}
---
+++ src/components/MyPageContent/MyBookList.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class MyBookList extends Component {
+    render() {
+        return (
+            <div>
+                2. myBookList Contents!!
+            </div>
+        )
+    }
+}
