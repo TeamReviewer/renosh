d61b0693e6dfa09424a8e1125cdaa11fadbd0cb3
Eunji Kim <eunk@microsoft.com>
2020-08-12 10:35:11 +0900

    implement profile loading
    Co-authored-by: citrusj0123@gmail.com <citrusj0123@gmail.com>

--- src/components/MyPage/MyPageTOCBody.jsx
+++ src/components/MyPage/MyPageTOCBody.jsx
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 import './MyPageTOCBody.css'
 import MyAnnoList from '../MyPageContent/MyAnnoList';
 import MyBookList from '../MyPageContent/MyBookList';
-import MyInfoChange from '../MyPageContent/MyInfoChange';
+import MyInfoChangeContainer from '../../containers/MyPageContent/MyInfoChange';
 
 export default class MyPageTOCBody extends Component {
     render() {
         let body;
         if(this.props.mode === 'myInfoChange') {
-            body = <MyInfoChange></MyInfoChange>            
+            body = <MyInfoChangeContainer></MyInfoChangeContainer>
         } else if(this.props.mode === 'myBookList') {
             body = <MyBookList></MyBookList>
         } else if(this.props.mode === 'myAnnoList') {
