96d57d579bad37f47a2d0c5bf4204ddc6fd7d278
ljh95 <ilegwmam@gmail.com>
2020-08-18 22:04:33 +0900

    delete sorting annoList

--- src/containers/PanelPage/AnnoList.jsx
+++ src/containers/PanelPage/AnnoList.jsx
@@ -5,7 +5,7 @@ export default connect(
     function(state) {
         return {
             id: state.selected_book_id,
-            annoList: state.selected_annoList.sort((o1, o2) => o2._ts - o1._ts),// 그런데 이렇게 조회할 때 마다 정렬을 해야할까? 뭔가 굉장히 리소스적으로 안좋은 형태인 것 같다.
+            annoList: state.selected_annoList,
         }
     },
     null
