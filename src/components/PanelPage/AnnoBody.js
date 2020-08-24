499548ac7e14cb87f4ceb6b4e82826a8897b8d7e
ljh95 <ilegwmam@gmail.com>
2020-08-24 21:17:11 +0900

    179/jongho/setAnnoPrivate

--- src/components/PanelPage/AnnoBody.js
+++ src/components/PanelPage/AnnoBody.js
@@ -11,14 +11,26 @@ class AnnoBody extends Component {
     handleMemoSubmit =  async (e) => {
         e.preventDefault(); // 페이지 리로딩 방지
         e.persist(); // 비동기적으로 이벤트 속성을 참조하고 싶을 때
-
-        await axios({
-            method:'put',
-            url: process.env.REACT_APP_RENOSH_BASE_URL + "api/highlights/"+this.props.book_id+ "/"+this.props.high_id,
-            data:{
-                memo: e.target.form.elements[0].value
-            }
-        });    
+        let isPublickChecked = e.target.form.elements[1].checked;
+        if(!isPublickChecked){
+            await axios({
+                method:'put',
+                url: process.env.REACT_APP_RENOSH_BASE_URL + "api/highlights/"+this.props.book_id+ "/"+this.props.high_id,
+                data:{
+                    memo: e.target.form.elements[0].value,
+                    scope: 'private'
+                }
+            });
+        } else {
+            await axios({                
+                method:'put',
+                url: process.env.REACT_APP_RENOSH_BASE_URL + "api/highlights/"+this.props.book_id+ "/"+this.props.high_id,
+                data:{
+                    memo: e.target.form.elements[0].value,
+                    scope: 'public'
+                }
+            });
+        }
 
         // 바뀐 내용이 바로 rendering될 수 있도록 annoList의 내용을 업데이트 해준다.
         let annoList = await this.getAnnoData(); 
@@ -39,7 +51,7 @@ class AnnoBody extends Component {
                     {high_text}
                     <input id="inputAnno" 
                         name="inputAnno" placeholder="content" autoFocus/>
-                    <input type="checkbox" name="Private" />Private
+                    <input type="checkbox" name="isPublic" />Public
                     <button id="saveAnno" onClick={this.handleMemoSubmit.bind(this)}>Save</button>
                 </form>  
                 <SelectAnnoListArrangeContainer />      
--- src/components/PanelPage/SelectAnnoListArrange.jsx
+++ src/components/PanelPage/SelectAnnoListArrange.jsx
@@ -6,13 +6,10 @@ export default class SelectAnnoListArrange extends Component {
     getAnnoData = async (viewMode) => {
         let book_id = this.props.id;  
         if(viewMode === 'private' && this.props.user_id !== null) {
-            const annos = await axios.get(process.env.REACT_APP_RENOSH_BASE_URL+"api/users/"+this.props.user_id+"/highlights");
+            const annos = await axios.get(process.env.REACT_APP_RENOSH_BASE_URL+"api/users/"+this.props.user_id+"/books/"+book_id+"/highlights");
             return annos.data;
-        }else if(viewMode === 'private' && this.props.user_id === null) {
-            const annos = await axios.get(process.env.REACT_APP_RENOSH_BASE_URL+"api/highlights/book/" + book_id);
-            return annos.data;
-        }else if(viewMode === 'others') {
-            const annos = await axios.get(process.env.REACT_APP_RENOSH_BASE_URL+"api/highlights/book/" + book_id);
+        }else if(viewMode === 'others' || (this.props.user_id === null && viewMode === 'private')) {
+            const annos = await axios.get(process.env.REACT_APP_RENOSH_BASE_URL+"api/highlights/book/" + book_id+ "/public");
             return annos.data;
         }
         else {
@@ -36,7 +33,7 @@ export default class SelectAnnoListArrange extends Component {
                             let annoList = await this.getAnnoData(viewMode); 
                             this.props.changeViweMode("CHANGE_VIEW_MODE", annoList)
                         }} name="arrange-display" value="private" />
-                        <label>Private</label>
+                        <label>Mine</label>
 
                         <input type="radio" id="others" onClick={async () => {
                             let viewMode = 'others'
