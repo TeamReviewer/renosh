7fc763855abd5c8d605a9ccded485187eb9fe0b6
ljh95 <ilegwmam@gmail.com>
2020-08-26 14:58:41 +0900

    500_test_scroll_move

--- src/components/PanelPage/Panel.js
+++ src/components/PanelPage/Panel.js
@@ -1,16 +1,20 @@
 import React, {Component} from 'react';
 import BookBriefInfoContainer from '../../containers/PanelPage/BookBriefInfo'
-// import AnnoBody from './AnnoBody';
 import AnnoBodyContainer from '../../containers/PanelPage/AnnoBody'
 import './Panel.css'
 
 
 class Panel extends Component {
+    findAnno() {
+        let scroll = this.refs.scrollElement || document.getElementById('scrollElement');
+        scroll.scrollTop = 2000;
+    }
     render() {
+        
         return(
-            <div id="panel">                
+            <div id="panel" ref="scrollElement" >
                 <BookBriefInfoContainer /> 
-                <AnnoBodyContainer changeLocation={this.props.changeLocation} />                
+                <AnnoBodyContainer changeLocation={this.props.changeLocation} findAnno={this.findAnno}/>                
             </div>
         )
     }
