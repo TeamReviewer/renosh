66262bdaba3c54697ea3707879335f14a40cb4d6
ljh95 <ilegwmam@gmail.com>
2020-08-26 14:54:53 +0900

    514/duplcation_delete

--- src/components/EpubViewerPage/EpubViewer.js
+++ src/components/EpubViewerPage/EpubViewer.js
@@ -9,6 +9,7 @@ import {
 } from "react-reader";
 import LinkButton from '../TagComponents/LinkButton'
 import "./EpubViewer.css"
+
 class EpubViewer extends Component {
   constructor(props) {
     super(props);
@@ -31,17 +32,17 @@ class EpubViewer extends Component {
 
   getAnnoData = async () => {
       let book_id = this.props.id;
-      const annos = await axios.get(process.env.REACT_APP_RENOSH_BASE_URL + "api/highlights/book/" + book_id);
+      const annos = await axios.get(process.env.REACT_APP_RENOSH_BASE_URL+"api/highlights/book/" + book_id+ "/public");
       return annos.data;
   }
   
   getRendition = rendition => {
     // Set inital font-size, and add a pointer to rendition for later updates
     const { largeText } = this.state;
-    
     this.rendition = rendition;
     rendition.themes.fontSize(largeText ? "140%" : "100%");
-
+    
+    // redux에 저장되어 있는 annoList에 대해 highlight를 그려주고 이벤트 옵션을 만들어 주는 부분입니다.
     if(this.props.annoList.length !== 0){
       if(this.props.annoList[0].bookid === this.props.id){
         for (let i = 0; i < this.props.annoList.length; i++) {      
@@ -49,23 +50,36 @@ class EpubViewer extends Component {
           let cfiRange = anno.location;
 
           if(this.rendition.epubcfi.isCfiString(cfiRange))
-            rendition.annotations.highlight(cfiRange);
+            rendition.annotations.add("highlight", cfiRange, {"id":anno.id}, (e)=>{
+              // 현재 패널이 열리는 옵션에 대해 수정이 필요합니다.
+                // if(!this.state.isPanelOpen)
+                //   this.handlePanelOpen();
+            }, 'test', ({"fill": "red", "fill-opacity": "1"}))
         }
       }
     }
-    
-    
 
-    // text를 드래깅했을 때 표시
+    // 새로 highlight를 만들 때 이용하는 메서드 입니다.
     this.rendition.on("selected", function (cfiRange, contents) {
-      
-      rendition.annotations.highlight(cfiRange, {}, (e) => {
-        console.log("highlight clicked", e.target);
-      });
+      rendition.annotations.add("highlight", cfiRange, {}, (e) => {
+        // if(!this.state.isPanelOpen)
+        //     this.handlePanelOpen();
+      }, 'test', ({"fill": "red", "fill-opacity": "1"}));
       contents.window.getSelection().removeAllRanges();
-    });
+    });    
 
-    // text를 드래깅했을 때 DB에 저장
+    // rendition의 기본 테마를 조정할 수 있는 것 같은데 이상하게 highlight에 대한 색상 변경은 위의 코드 (55, 63)과 같이 해야합니다.
+    this.rendition.themes.default({
+      // 드래그 했을 때의 배경색 지정!
+      '::selection': {
+        'background': 'rgba(255,255, 0, 0.3)'
+      },
+      // 무슨 역할인지 모르겠습니다. 없어도 무방한 것 같습니다.
+      '.epubjs-hl' : {
+        'fill': 'black', 'fill-opacity': '0.3', 'mix-blend-mode': 'multiply'
+      }
+    });
+        
     this.rendition.on("selected", async function (cfiRange) {
       this.rendition.book.getRange(cfiRange).then( async function (range) {
         var text;
@@ -110,6 +124,7 @@ class EpubViewer extends Component {
   }
 
   render() {   
+    console.log("EpubViewer is rendering")
     return (
       <div>
         <div>
