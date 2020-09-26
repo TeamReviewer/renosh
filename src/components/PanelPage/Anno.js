cc8048bc9302aad227579ee0def7d1ad050e6424
TenaKim <tena.kim10@gmail.com>
2020-09-27 04:14:36 +0900

    Fix the delete button and update button on the panel and arrange

--- src/components/PanelPage/Anno.js
+++ src/components/PanelPage/Anno.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import { Col } from 'antd';
+import { Col, Row } from 'antd';
+import { SmileTwoTone, FormOutlined, DeleteOutlined } from '@ant-design/icons';
 import './anno.less'
 import axios from 'axios';
 
@@ -20,24 +21,33 @@ class Anno extends Component {
                     .then(res => {
                         this.props.deleteAnno("DELETE_ANNO", this.props.anno_id)
                     })
-            }}>delete</button>
+            }}><DeleteOutlined /></button>
             updateButton = <button onClick={e => {
                 //패널을 가장위로 올리고
                 this.props.findAnno(0)
                 // form에 inputAnno의 내용을 채워 넣는다.
                 this.props.updateAnnoRequest(this.props.memo, this.props.anno_id)
-            }}>update</button>
+            }}><FormOutlined /></button>
         }
         return (
             <Col id="panelAnno">
-                <span id="username">{this.props.username ? this.props.username : ""}</span>
-                <span id="memo">{this.props.memo ? this.props.memo : ""}</span>
-                {updateButton}
-                {deleteButton}
-                <span id="text" onClick={function (e) {
-                    this.props.moveToAnno("MOVE_EPUB", this.props.cfiRange)
-                    this.props.changeLocation(this.props.cfiRange)
-                }.bind(this)}>{this.props.text ? this.props.text : ""}</span>
+                <Row id="panelProfile">
+                    <Col id="userPhoto"><SmileTwoTone /></Col>
+                    <Col id="username">{this.props.username ? this.props.username : ""}</Col>
+                    <Col id="annoButton">
+                        <div id="updateButton">{updateButton}</div>
+                        <div id="deleteButton">{deleteButton}</div>
+                    </Col>
+                </Row>
+                <Row>
+                    <span id="memo">{this.props.memo ? this.props.memo : ""}</span>
+                </Row>
+                <Row>
+                    <span id="text" onClick={function (e) {
+                        this.props.moveToAnno("MOVE_EPUB", this.props.cfiRange)
+                        this.props.changeLocation(this.props.cfiRange)
+                    }.bind(this)}>{this.props.text ? this.props.text : ""}</span>
+                </Row>
             </Col>
         )
     }
