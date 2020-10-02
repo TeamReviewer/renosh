1fec34f8d2576175033ceacf46c711ffa7d26153
TenaKim <tena.kim10@gmail.com>
2020-10-02 12:55:39 +0900

    Delete the comments before deviding a HeaderBelow component

--- src/components/ListPage/BookListContent.jsx
+++ src/components/ListPage/BookListContent.jsx
@@ -16,21 +16,6 @@ class BookListContent extends React.Component{
                                 {/*<Col id="leftSider" xs={0} sm={2} md={4} lg={4} xl={4}></Col>*/}
                                 {/*<Col id="contentCenter" xs={24} sm={20} md={16} lg={16} xl={16}>*/}
                                 <Col id="contentCenter">
-                                    {/*
-                                    <Row id="contentButtons">
-                                        <div className="buttonsDiv">
-                                            <Link><LoginButton/></Link>
-                                            <Link to={'/myPage'}>
-                                                <Button type="text" icon={<BookOutlined />} style={{color: "#2b335b"}}>My Books</Button>
-                                            </Link>
-                                            <Link to={'/'}>
-                                                <Button type="text" icon={<HomeOutlined />} style={{color: "#2b335b", fontSize: "0.5"}}>Home</Button>
-                                            </Link>
-                                            <Link><Button type="text" icon={<SearchOutlined />} style={{color: "#2b335b"}}>Discover</Button></Link>
-                                            <Link><Button type="text" icon={<SettingOutlined />} style={{color: "#2b335b"}}>Settings</Button></Link>
-                                        </div>               
-                                    </Row>
-                                    */}
                                     <HeaderBelowComponent />
                                     <Row id="contentTitle">
                                         <span>Your Books</span>
